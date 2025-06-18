d723d1ba49be80b0f8b32f6d83dfb04f15d4022f
Doniyor <dkuzybaev@yandex.ru>
2025-06-18 20:15:36 +0300

    add fucntion

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-const cart = document.querySelectorAll('card');
-const modal = document.querySelector('modal');
+const cart = document.querySelectorAll('.card');
+const modal = document.querySelector('.modal');
 
 cart.forEach((item) => {
-  console.log(item);
+  item.addEventListener('click', (e) => {
+    if (modal.style.display === 'none') {
+      modal.style.display = 'block';
+    } else {
+      modal.style.display = 'none';
+    }
+  });
 });
